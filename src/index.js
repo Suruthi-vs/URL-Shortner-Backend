7d8a8421ffe58e3d669d11fd9e923b4569d36a42
Suruthi-vs <62285669+Suruthi-vs@users.noreply.github.com>
2020-10-13 16:33:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const chartroute= require("./routes/chartroute")
 app.use("/",userroute)
 app.use("/url",urlroute)
 app.use("/visual",chartroute)
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(process.env.PORT|| 3000,()=>{
   console.log("Server connected!!!")
 })
 
